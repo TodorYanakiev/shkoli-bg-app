1c4b46a295cf1a6ce8d5e2e47e65983cc678bf22
TodorYanakiev <todoryanakiev395@gmail.com>
2026-01-04 12:48:06 +0200

    refactor: remove AppHeader component and associated translations

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,14 +1,12 @@
 import { Outlet } from 'react-router-dom'
 
 import AppFooter from './components/AppFooter'
-import AppHeader from './components/AppHeader'
 import TopNav from './components/TopNav'
 
 const AppLayout = () => {
   return (
     <div className="flex min-h-screen flex-col bg-slate-50 text-slate-900">
       <TopNav />
-      <AppHeader />
       <main className="relative w-full flex-1 overflow-hidden px-4 py-8 sm:px-6 sm:py-10 lg:px-12">
         <div aria-hidden className="page-background" />
         <div className="relative z-10">
