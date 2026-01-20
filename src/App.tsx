cff31f87b0c4eb95ea328590c993e1aa3bd96d9f
TodorYanakiev <todoryanakiev395@gmail.com>
2026-01-20 13:37:41 +0200

    Wire auth-token-change listener to reset user-related Query cahe

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,17 @@ import { QueryClientProvider } from '@tanstack/react-query'
 
 import appRouter from './routes/App.router'
 import ToastProvider from './components/feedback/ToastProvider'
+import { useAuthQueryReset } from './hooks/useAuthQueryReset'
 import { queryClient } from './utils/queryClient'
+
+const AuthQuerySync = () => {
+  useAuthQueryReset()
+  return null
+}
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
+    <AuthQuerySync />
     <HelmetProvider>
       <ToastProvider>
         <RouterProvider router={appRouter} />
