917a0e9c1891ca8baa8b74aca44333c9a62beba8
TodorYanakiev <todoryanakiev395@gmail.com>
2026-02-16 01:19:07 +0200

    feat(map): integrate full-screen map layout styles and map i18n copy

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,20 +1,29 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 
 import { useConsentManagedTracking } from '../hooks/useConsentManagedTracking'
 import AppFooter from './components/AppFooter'
 import TopNav from './components/TopNav'
 
 const AppLayout = () => {
+  const location = useLocation()
+  const isMapRoute = location.pathname === '/map'
+
   useConsentManagedTracking()
 
   return (
     <div className="relative min-h-screen bg-transparent text-slate-900">
       <div className="relative z-10 flex min-h-screen flex-col">
         <TopNav />
-        <main className="relative w-full flex-1 overflow-visible pb-8 pt-[calc(var(--topnav-height,0px)+2rem)] sm:pb-10 sm:pt-[calc(var(--topnav-height,0px)+2.5rem)] pr-4 sm:pr-6 lg:pr-12 pl-[calc(1rem+var(--page-sidebar-offset,0px))] sm:pl-[calc(1.5rem+var(--page-sidebar-offset,0px))] lg:pl-[calc(3rem+var(--page-sidebar-offset,0px))]">
+        <main
+          className={
+            isMapRoute
+              ? 'relative w-full flex-1 overflow-hidden pt-[var(--topnav-height,0px)]'
+              : 'relative w-full flex-1 overflow-visible pb-8 pt-[calc(var(--topnav-height,0px)+2rem)] sm:pb-10 sm:pt-[calc(var(--topnav-height,0px)+2.5rem)] pr-4 sm:pr-6 lg:pr-12 pl-[calc(1rem+var(--page-sidebar-offset,0px))] sm:pl-[calc(1.5rem+var(--page-sidebar-offset,0px))] lg:pl-[calc(3rem+var(--page-sidebar-offset,0px))]'
+          }
+        >
           <Outlet />
         </main>
-        <AppFooter />
+        {isMapRoute ? null : <AppFooter />}
       </div>
     </div>
   )
