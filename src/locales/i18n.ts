54e0b280378582e4efb2d3bc20a7ee797355b9a7
TodorYanakiev <todoryanakiev395@gmail.com>
2026-03-11 23:39:37 +0200

    perf(i18n): lazy-load locale namespaces for smaller initial payload

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -1,42 +1,134 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 
-import enCommon from './en/common.json'
-import enLegal from './en/legal.json'
 import bgCommon from './bg/common.json'
-import bgLegal from './bg/legal.json'
 import {
   defaultLanguage,
   getInitialLanguage,
+  isSupportedLanguage,
   persistLanguage,
   supportedLanguages,
+  type LanguageCode,
 } from '../utils/language'
 
-const resources = {
-  en: { common: enCommon, legal: enLegal },
-  bg: { common: bgCommon, legal: bgLegal },
-} as const
+type TranslationNamespace = 'common' | 'legal'
+type TranslationBundle = Record<string, unknown>
 
-i18n.use(initReactI18next).init({
-  resources,
-  lng: getInitialLanguage(),
-  fallbackLng: defaultLanguage,
-  supportedLngs: supportedLanguages,
-  nonExplicitSupportedLngs: true,
-  defaultNS: 'common',
-  interpolation: { escapeValue: false },
-  react: { useSuspense: false },
-})
+const loadEnCommon = async (): Promise<TranslationBundle> => {
+  const module = await import('./en/common.json')
+  return module.default as TranslationBundle
+}
+
+const loadBgLegal = async (): Promise<TranslationBundle> => {
+  const module = await import('./bg/legal.json')
+  return module.default as TranslationBundle
+}
+
+const loadEnLegal = async (): Promise<TranslationBundle> => {
+  const module = await import('./en/legal.json')
+  return module.default as TranslationBundle
+}
+
+const namespaceLoaders: Record<
+  LanguageCode,
+  Record<TranslationNamespace, () => Promise<TranslationBundle>>
+> = {
+  bg: {
+    common: async () => bgCommon as TranslationBundle,
+    legal: loadBgLegal,
+  },
+  en: {
+    common: loadEnCommon,
+    legal: loadEnLegal,
+  },
+}
+
+const toNamespaceKey = (language: LanguageCode, namespace: TranslationNamespace) =>
+  `${language}:${namespace}`
+
+const loadedNamespaces = new Set<string>([toNamespaceKey('bg', 'common')])
+
+const toSupportedLanguage = (language: string): LanguageCode =>
+  isSupportedLanguage(language) ? language : defaultLanguage
+
+const loadNamespace = async (
+  language: LanguageCode,
+  namespace: TranslationNamespace,
+) => {
+  const cacheKey = toNamespaceKey(language, namespace)
+  if (
+    loadedNamespaces.has(cacheKey) ||
+    i18n.hasResourceBundle(language, namespace)
+  ) {
+    loadedNamespaces.add(cacheKey)
+    return
+  }
+
+  const bundle = await namespaceLoaders[language][namespace]()
+  i18n.addResourceBundle(language, namespace, bundle, true, true)
+  loadedNamespaces.add(cacheKey)
+}
+
+export const ensureI18nNamespaces = async (
+  language: string,
+  namespaces: readonly TranslationNamespace[],
+) => {
+  const resolvedLanguage = toSupportedLanguage(language)
+  await Promise.all(
+    namespaces.map((namespace) => loadNamespace(resolvedLanguage, namespace)),
+  )
+}
 
 const applyDocumentLanguage = (language: string) => {
   if (typeof document === 'undefined') return
   document.documentElement.lang = language
 }
 
-applyDocumentLanguage(i18n.language)
-i18n.on('languageChanged', (language) => {
+const onLanguageChanged = (language: string) => {
   applyDocumentLanguage(language)
   persistLanguage(language)
-})
+}
+
+const originalChangeLanguage = i18n.changeLanguage.bind(i18n)
+
+i18n.changeLanguage = async (language, callback) => {
+  if (typeof language === 'string' && isSupportedLanguage(language)) {
+    await ensureI18nNamespaces(language, ['common'])
+  }
+
+  return originalChangeLanguage(language, callback)
+}
+
+export const i18nReady = i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      bg: { common: bgCommon },
+    },
+    lng: defaultLanguage,
+    fallbackLng: defaultLanguage,
+    supportedLngs: supportedLanguages,
+    nonExplicitSupportedLngs: true,
+    defaultNS: 'common',
+    ns: ['common', 'legal'],
+    interpolation: { escapeValue: false },
+    react: { useSuspense: false },
+  })
+  .then(async () => {
+    i18n.on('languageChanged', onLanguageChanged)
+
+    const initialLanguage = getInitialLanguage()
+    await ensureI18nNamespaces(initialLanguage, ['common'])
+
+    if (i18n.language !== initialLanguage) {
+      await i18n.changeLanguage(initialLanguage)
+      return
+    }
+
+    onLanguageChanged(initialLanguage)
+  })
+  .catch(() => {
+    onLanguageChanged(defaultLanguage)
+  })
 
 export default i18n
